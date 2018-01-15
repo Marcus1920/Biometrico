81f76600ff0dc327a61df5aed5cc0872540f6af7
mozaamisi <21335890@dut4life.ac.za>
2018-01-15 10:58:25 +0200

    updates

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->integer('company_id')->unsigned();
+            $table->integer('role')->unsigned();
+            $table->string('cellphone');
+            $table->string('email');
+            $table->string('password');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('role')->references('id')->on('roles');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
